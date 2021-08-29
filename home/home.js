1a0797224e0eba2b8e0da728e763fb206a61aae0
LegalPirates2020 <legalpirates2020@gmail.com>
2021-08-29 19:48:51 +0530

    Changes

--- home/home.js
+++ home/home.js
@@ -1,4 +1,29 @@
-const planeHero = document.querySelector('.plane-hero');
-const topNav = document.querySelector('.top-nav');
-const heroContent = document.querySelector('.hero-content');
-const shkAviation = document.querySelector('.SHK-aviation');
+const swiper = new Swiper('.mySwiper', {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	loop: true,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	}
+});
+
+gsap.from('.hero-content', {
+	duration: 1,
+	height: 0,
+	ease: 'Power3.easeInOut'
+});
+
+const t1 = gsap.timeline({
+	scrollTrigger: { trigger: '.bottom-nav', start: 'bottom top' }
+});
+
+t1.from('.SHK-title', {
+	x: 250,
+	opacity: 0,
+	duration: 1
+});
