d7e91d76e8f677150f21403af915ee5d73527cc3
LegalPirates2020 <legalpirates2020@gmail.com>
2021-08-30 14:49:44 +0530

    Change

--- home/home.js
+++ home/home.js
@@ -26,4 +26,34 @@ t1.from('.SHK-title', {
 	x: 250,
 	opacity: 0,
 	duration: 1
+}).from(
+	'.SHK-para',
+	{
+		duration: 1,
+		opacity: 0,
+		x: -250
+	},
+	'-=1	'
+);
+
+const t2 = gsap.timeline({
+	scrollTrigger: {
+		trigger: '.hero',
+		start: 'bottom center'
+	}
 });
+
+t2.to('.plane-hero', { duration: 0.5, rotate: 90, ease: 'none' })
+	.to('.plane-hero', {
+		duration: 1,
+		x: 800,
+		delay: 0.5,
+		ease: 'none'
+	})
+	.to('.plane-hero', {
+		duration: 1,
+		rotate: 180,
+		delay: 0.5,
+		ease: 'none'
+	})
+	.to('.hero-content', { duration: 1, x: -225 }, '-=2');
