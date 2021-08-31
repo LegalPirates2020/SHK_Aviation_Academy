945285b76729f86fbbe5b5b082d6e561c5cea31f
LegalPirates2020 <legalpirates2020@gmail.com>
2021-08-31 19:18:09 +0530

    Changes

--- home/home.js
+++ home/home.js
@@ -12,54 +12,94 @@ const swiper = new Swiper('.mySwiper', {
 	}
 });
 
+const inputs = document.querySelectorAll('.input');
+const button = document.querySelector('.submit');
+const form = document.querySelector('form');
+const nextSubmit = document.querySelector('.next-submit');
+const textArea = document.querySelector('.text-area');
+
+inputs.forEach((input) => {
+	input.addEventListener('input', (e) => {
+		if (inputs[2].value == e.data) {
+			if (inputs[0].value.length && inputs[1].value.length) {
+				nextSubmit.style.left = '55.5rem';
+			} else {
+				nextSubmit.style.left = '0';
+			}
+		} else {
+			nextSubmit.style.left = '0';
+			button.style.left = '0';
+		}
+	});
+});
+
+nextSubmit.addEventListener('click', () => {
+	inputs.forEach((input) => {
+		input.style.left = '-100%';
+		input.style.left = '-100%';
+		nextSubmit.style.left = '0';
+	});
+	textArea.style.left = '44rem';
+});
+
+textArea.addEventListener('keydown', (e) => {
+	if (textArea.value.length >= 30) {
+		button.style.left = '60rem';
+		button.removeAttribute('disabled');
+	}
+});
+
 gsap.from('.hero-content', {
 	duration: 1,
 	height: 0,
 	ease: 'Power3.easeInOut'
 });
 
-const t1 = gsap.timeline({
-	scrollTrigger: { trigger: '.bottom-nav', start: 'bottom top' }
-});
-
-t1.from('.SHK-title', {
-	x: 250,
-	opacity: 0,
-	duration: 1
-}).from(
-	'.SHK-para',
-	{
-		duration: 1,
+gsap
+	.timeline({
+		scrollTrigger: { trigger: '.bottom-nav', start: 'bottom top' }
+	})
+	.from('.SHK-title', {
+		x: 250,
 		opacity: 0,
-		x: -250
-	},
-	'-=1	'
-);
+		duration: 1
+	})
+	.from(
+		'.SHK-para',
+		{
+			duration: 1,
+			opacity: 0,
+			x: -250
+		},
+		'-=1	'
+	);
 
-const t2 = gsap.timeline({
-	scrollTrigger: {
-		trigger: '.swiper',
-		start: 'top 40%'
-	}
-});
+gsap
+	.timeline({
+		scrollTrigger: {
+			trigger: '.swiper',
+			start: 'top 40%'
+		}
+	})
+	.from('.swiper', {
+		duration: 1,
+		width: '100%',
+		height: '100%',
+		borderRadius: '0'
+	})
+	.from('.slide-h1', { duration: 1, height: 0 }, '-=.5');
 
-t2.from('.swiper', { duration: 1, width: '100%', height: '100%' }).from(
-	'.slide-h1',
-	{ duration: 1, height: 0 },
-	'-=.5'
-);
-
-const t3 = gsap.timeline({
-	scrollTrigger: {
-		trigger: '.vision-and-mission',
-		start: 'top 40%'
-	}
-});
-
-t3.from('.vision-h1', {
-	duration: 0.5,
-	height: 0
-})
+gsap
+	.timeline({
+		scrollTrigger: {
+			trigger: '.vision-and-mission',
+			start: 'top 40%'
+		}
+	})
+	.from('.vision-h1', {
+		duration: 0.5,
+		height: 0
+	})
 	.from(
 		'.vision-p',
 		{
@@ -87,28 +127,35 @@ t3.from('.vision-h1', {
 		'-=.5'
 	);
 
-// const t2 = gsap.timeline({
-// 	scrollTrigger: {
-// 		trigger: '.hero',
-// 		start: 'bottom top'
-// 	}
-// });
+gsap
+	.timeline({
+		scrollTrigger: {
+			trigger: '.SHK-heads',
+			start: 'top 40%'
+		}
+	})
+	.from('.heads', { duration: 0.5, scaleY: 0 })
+	.from(
+		'.head-content-title',
+		{
+			duration: 0.5,
+			height: 0
+		},
+		'-=.5'
+	)
+	.from(
+		'.head-content-para',
+		{
+			duration: 0.5,
+			x: 200,
+			opacity: 0
+		},
+		'-=.5'
+	);
 
-// t2.to('.plane-hero', { duration: 1, rotate: 90, ease: 'power1' })
-// t2.to('.plane-hero', {
-// duration: 0.7,
-// x: 800,
-// delay: 0.5,
-// 	ease: 'none'
-// });
-// .to(
-// 	'.plane-hero',
-// 	{
-// 		duration: 0.5,
-// 		rotate: 180,
-// 		delay: 0.5,
-// 		ease: 'power1'
-// 	},
-// 	'-=.15'
-// )
-// .to('.hero-content', { duration: 1, x: -225, ease: 'power1' }, '-=2');
+gsap.from('.input', {
+	scrollTrigger: { trigger: '.contact-sec', start: 'top 40%' },
+	duration: 0.05,
+	x: -700,
+	stagger: 0.2
+});
